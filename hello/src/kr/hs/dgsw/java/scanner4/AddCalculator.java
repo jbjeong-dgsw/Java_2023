b32cd87445648f3aff8b2fe97c9ab25898a7e9e2
김동균 <nabilera1@naver.com>
2023-03-30 12:30:14 +0900

    3월 30일 4반 수업

--- hello/src/kr/hs/dgsw/java/scanner4/AddCalculator.java
+++ hello/src/kr/hs/dgsw/java/scanner4/AddCalculator.java
@@ -1,7 +1,5 @@
 package kr.hs.dgsw.java.scanner4;
 
-import kr.hs.dgsw.java.ScannerStudy;
-
 import java.util.Scanner;
 
 public class AddCalculator {
@@ -11,13 +9,17 @@ public class AddCalculator {
         this.scanner = new Scanner(System.in);
     }
 
-    public void calculate() {
+    public void execute() {
         System.out.println("두 개의 정수를 입력하세요.");
         int value1 = this.scanner.nextInt();
         int value2 = this.scanner.nextInt();
-        int result = value1 + value2;
+        int result = execute(value1, value2);
 
         System.out.printf("%d + %d = %d\n", value1, value2, result);
+    }
+
+    public int execute(int value1, int value2) {
+        return value1 + value2;
     }
 
     public void close() {
@@ -37,7 +39,7 @@ public class AddCalculator {
 
         AddCalculator calculator = new AddCalculator();
         calculator.prepare();
-        calculator.calculate();
+        calculator.execute();
         calculator.close();
     }
 }
