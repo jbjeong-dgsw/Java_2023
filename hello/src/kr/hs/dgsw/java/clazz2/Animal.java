30f251b9af225e1d3b45d863dc925a9979e25e09
김동균 <nabilera1@naver.com>
2023-04-19 11:25:34 +0900

    4월 19일 2반

---
+++ hello/src/kr/hs/dgsw/java/clazz2/Animal.java
@@ -0,0 +1,41 @@
+package kr.hs.dgsw.java.clazz2;
+
+public class Animal {
+    public String name;
+
+    public static int age;
+
+    public Animal() {
+
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printData() {
+        System.out.printf("%s : $d\n", name, age);
+    }
+
+    public static void showData() {
+        //System.out.printf("%s : $d\n", name, age);
+    }
+
+
+}
---
+++ hello/src/kr/hs/dgsw/java/clazz2/Doer.java
@@ -0,0 +1,29 @@
+package kr.hs.dgsw.java.clazz2;
+
+public class Doer {
+
+    public static void main(String[] args) {
+        Animal lion = new Animal("사자");
+        //lion.setName("사자");
+        lion.setAge(10);
+
+        Animal rabbit = new Animal();
+        rabbit.setName("토끼");
+        rabbit.setAge(5);
+
+        rabbit.age = 12;
+        Animal.age = 15;
+
+        rabbit.name = "코끼리";
+        //Animal.name = "코끼리";
+
+        System.out.printf("%s 의 나이는 %d 살입니다.\n",
+                lion.getName(), lion.getAge());
+
+        System.out.printf("%s 의 나이는 %d 살입니다.\n",
+                rabbit.getName(), rabbit.getAge());
+
+
+    }
+
+}
