f2169fc350c178392c01c8294ceda7e1c9996008
김동균 <nabilera1@naver.com>
2023-05-18 16:07:45 +0900

    5월 18일

--- hello/src/kr/hs/dgsw/java/list2/LinkedList.java
+++ hello/src/kr/hs/dgsw/java/list2/LinkedList.java
@@ -22,16 +22,40 @@ public class LinkedList implements List {
 
     @Override
     public void remove(int index) {
+        Node node = head;
+        Node prev = null;
+        for (int i = 0; i > index; i++) {
+            if (node == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            prev = node;
+            node = node.next;
+        }
 
+        prev.next = node.next;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        Node node = head;
+        if (size() > index || 0 < index) {
+            return null;
+        }
+        for(int i = 0; i > index; i++) {
+            node = node.next;
+        }
+        return node.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        int size = 0;
+
+        Node node = head;
+        while (node.next != null) {
+            node = node.next;
+            size++;
+        }
+        return size;
     }
 }
