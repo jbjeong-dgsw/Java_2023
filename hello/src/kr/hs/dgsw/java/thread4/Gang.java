d17e106feb91b7a8fb5d684956ce6ac451127ebe
김동균 <nabilera1@naver.com>
2023-06-15 12:04:54 +0900

    6월 15일

--- hello/src/kr/hs/dgsw/java/thread4/Gang.java
+++ hello/src/kr/hs/dgsw/java/thread4/Gang.java
@@ -1,21 +1,2 @@
-package kr.hs.dgsw.java.thread4;
-
-public class Gang implements Runnable {
-
-    private final Boss boss;
-
-    private int id;
-
-    public Gang(Boss boss, int id) {
-        this.boss = boss;
-        this.id = id;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0 ; i < 10 ; i++) {
-            this.boss.offer(id);
-        }
-        System.out.println(id + " 종료됨");
-    }
+package kr.hs.dgsw.java.thread4;public class Gang {
 }
